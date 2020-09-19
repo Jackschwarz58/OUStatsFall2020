c1330bd843860f99cd4ff01b1952630333fcfa7b
Jack Schwarz <jackschwarz58@gmail.com>
2020-09-18 23:43:11 -0500

    added package for lab 4

---
+++ quad/R/quad.R
@@ -0,0 +1,10 @@
+#' @@title quad
+#'
+#' @param x, a, b, c - params for the quatric function y=mx+b
+#'
+#' @description Returns the result of the function y=mx+b with given params. Just for a lab assigned exmaple
+#' @return Number Value
+#'
+quad = function(x, a, b, c) {
+  return(a * x^2 + b * x + c)
+}
